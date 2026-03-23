a17e1a7f640d73ab6d1ad7d488b5ee57fb253804
Kashif Naveed <knaveed@blueiberis.com.au>
2026-03-23 19:00:55 +1000

    Fix Imports

--- frontend/lib/aws-exports.ts
+++ frontend/lib/aws-exports.ts
@@ -1,9 +1,3 @@
 import { Amplify } from "aws-amplify";
 
-Amplify.configure({
-  Auth: {
-    region: process.env.NEXT_PUBLIC_REGION || "us-east-1",
-    userPoolId: process.env.NEXT_PUBLIC_USER_POOL_ID!,
-    userPoolWebClientId: process.env.NEXT_PUBLIC_USER_POOL_CLIENT_ID!,
-  },
-});
+Amplify.configure({});
