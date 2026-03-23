899178970aee7b4189699dae46d3c05fb9d71ca8
Kashif Naveed <knaveed@blueiberis.com.au>
2026-03-23 19:43:11 +1000

    Add Env as Suffix

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { InfraStack } from '../lib/infra-stack';
+const envCapitalized = process.env.AWS_ACCOUNT_ENV_CAPITALIZED ?? 'Prod';
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack');
+new InfraStack(app, `Notoa${envCapitalized}`);
